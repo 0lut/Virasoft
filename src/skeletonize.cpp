9a1f3388fa17afef6b35eb77f8b50d7a02adf141
norveclibalikci <oluts@itu.edu.tr>
2017-07-21 16:54:29 +0300

    ADD Skeletonize

---
+++ src/skeletonize.cpp
@@ -0,0 +1,6 @@
+#include "skeletonize.h"
+
+skeletonize::skeletonize()
+{
+
+}
---
+++ src/skeletonize.h
@@ -0,0 +1,11 @@
+#ifndef SKELETONIZE_H
+#define SKELETONIZE_H
+
+
+class skeletonize
+{
+public:
+    skeletonize();
+};
+
+#endif // SKELETONIZE_H
